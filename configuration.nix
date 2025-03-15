b32942facedb32e4c70041d515583b257b5da44d
ingenarel <ingenarelitems@gmail.com>
2025-03-16 05:00:12 +0600

    (config): made a better mpd config

--- configuration.nix
+++ configuration.nix
@@ -189,6 +189,18 @@
     mpd = {
       enable = true;
       musicDirectory = "/home/ingenarel/Music";
+      user = "ingenarel";
+      extraConfig = ''        db_file            "/home/ingenarel/Music/mpd/database"
+
+        # Uncomment to refresh the database whenever files in the music_directory are changed
+        auto_update "yes"
+
+        # Uncomment to enable the functionalities
+        playlist_directory "/home/ingenarel/Music/mpd/playlists"
+        pid_file           "/home/ingenarel/Music/mpd/pid"
+        state_file         "/home/ingenarel/Music/state"
+        sticker_file       "/home/ingenarel/Music/mpd/sticker.sql"
+      '';
     };
   };
 
