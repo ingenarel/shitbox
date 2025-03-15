44664ebad250bd31025a5a02c07ae70cd153dd04
ingenarel <ingenarelitems@gmail.com>
2025-03-15 20:40:18 +0600

    (audio): enable some functionalities

--- configuration.nix
+++ configuration.nix
@@ -28,6 +28,12 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    jack.enable = true;
+    wireplumber.enable = true;
   };
 
   virtualisation.waydroid.enable = true;
