8a154eead369dad073356d14b05180c1ce815847
ingenarel <ingenarelitems@gmail.com>
2025-03-16 14:08:26 +0600

    (auto): update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "A very basic flake";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable-small";
   };
 
