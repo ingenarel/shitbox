5f2bb693b91ed4bea16c383a04cb74f3d76d98cc
ingenarel <ingenarelitems@gmail.com>
2025-03-17 16:44:57 +0600

    (misc): add vim modeline

--- configuration.nix
+++ configuration.nix
@@ -1,3 +1,4 @@
+# vim: set ts=2 sw=2 sts=2:
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
