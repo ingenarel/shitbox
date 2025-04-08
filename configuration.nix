0e505f586c1b3c0a11b0aa74cafeb0c9ad8fd016
ingenarel <ingenarelitems@gmail.com>
2025-04-09 03:01:21 +0600

    (misc): seperated mpd config to a seperate file

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
     pkgs,
     ...
 }: {
-    imports = [./hardware-configuration.nix ./configs/hyprland/init.nix];
+    imports = [./hardware-configuration.nix ./configs/hyprland/init.nix ./configs/mpd/init.nix];
 
     boot.loader.grub = {
         device = "/dev/sda";
@@ -81,7 +81,6 @@
         lutris
         mediainfo
         mpc
-        mpd
         mtpfs
         mutt-wizard
         ncmpcpp
@@ -193,27 +192,6 @@
                 };
             };
         };
-        mpd = {
-            enable = true;
-            musicDirectory = "/home/ingenarel/Music";
-            user = "ingenarel";
-            extraConfig = ''
-              db_file            "/home/ingenarel/Music/mpd/database"
-
-              # Uncomment to refresh the database whenever files in the music_directory are changed
-              auto_update "yes"
-
-              # Uncomment to enable the functionalities
-              playlist_directory "/home/ingenarel/Music/mpd/playlists"
-              pid_file           "/home/ingenarel/Music/mpd/pid"
-              state_file         "/home/ingenarel/Music/state"
-              sticker_file       "/home/ingenarel/Music/mpd/sticker.sql"
-              audio_output {
-                type "pipewire"
-                name "My PipeWire Output"
-              }
-            '';
-        };
         pipewire = {
             enable = true;
             pulse.enable = true;
@@ -230,9 +208,6 @@
     systemd.services = {
         greetd = {
             restartIfChanged = false;
-        };
-        mpd.environment = {
-            XDG_RUNTIME_DIR = "/run/user/1000";
         };
     };
     hardware.graphics = {
