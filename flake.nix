953771585a22d38bbd3060e1d15cdde7e733d03a
ingenarel <ingenarelitems@gmail.com>
2025-04-09 15:40:27 +0600

    (feat): added hyprland-plugins flake

--- flake.nix
+++ flake.nix
@@ -7,10 +7,10 @@
             url = "github:hyprwm/Hyprland";
             inputs.nixpkgs.follows = "nixpkgs";
         };
-        # hyprland-plugins = {
-        #   url = "github:hyprwm/hyprland-plugins";
-        #   inputs.hyprland.follows = "hyprland";
-        # };
+        hyprland-plugins = {
+            url = "github:hyprwm/hyprland-plugins";
+            inputs.hyprland.follows = "hyprland";
+        };
         zen-browser = {
             url = "github:0xc000022070/zen-browser-flake";
             inputs.nixpkgs.follows = "nixpkgs";
