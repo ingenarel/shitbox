bcd702ee0e07c341ee5cc048b46ea77d2f499194
ingenarel <ingenarelitems@gmail.com>
2025-04-10 19:12:13 +0600

    (misc): moved script paths

--- configs/programs/hyprland/init.nix
+++ configs/programs/hyprland/init.nix
@@ -12,11 +12,18 @@
         };
         hyprlock.enable = true;
     };
-    home-manager.users.ingenarel.wayland.windowManager.hyprland = {
-        enable = true;
-        package = null;
-        portalPackage = null;
-        plugins = [inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprexpo];
-        extraConfig = builtins.readFile ./config/hyprland.conf;
+    home-manager.users.ingenarel = {
+        wayland.windowManager.hyprland = {
+            enable = true;
+            package = null;
+            portalPackage = null;
+            plugins = [inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprexpo];
+            extraConfig = builtins.readFile ./config/hyprland.conf;
+        };
+        home.file = {
+            ".config/hypr/notes".source = ./config/notes;
+            ".config/hypr/scripts".source = ./config/scripts;
+            ".config/hypr/hyprlock.conf".source = ./config/hyprlock.conf;
+        };
     };
 }
