eda460ebfd25574ac7b33defce41eb4b8263c1c8
ingenarel <ingenarelitems@gmail.com>
2025-04-11 21:11:00 +0600

    (feat): enable cachix for nix

--- configs/programs/hyprland/init.nix
+++ configs/programs/hyprland/init.nix
@@ -12,6 +12,10 @@
         };
         hyprlock.enable = true;
     };
+    nix.settings = {
+        substituters = ["https://hyprland.cachix.org"];
+        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
     home-manager.users.ingenarel = {
         wayland.windowManager.hyprland = {
             enable = true;
