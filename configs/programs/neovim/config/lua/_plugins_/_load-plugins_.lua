fdfa59f900a7d110c71f170c7e87ad3685ca040f
ingenarel <ingenarelitems@gmail.com>
2025-04-11 17:30:16 +0600

    (fix): 626417e270cd

--- configs/programs/neovim/config/lua/_plugins_/_load-plugins_.lua
+++ configs/programs/neovim/config/lua/_plugins_/_load-plugins_.lua
@@ -20,7 +20,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup {
     -- TODO: download the profiler plugin somehow (https://github.com/folke/snacks.nvim/blob/main/docs/profiler.md)
     -- TODO: download neovim tmux navigator (https://github.com/alexghergh/nvim-tmux-navigation) or create my own where the stupid c-l c-h isn't predefined
-    lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json"
+    lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json",
     spec = {
         {
             require("_plugins_._dashboard-setup_"),
