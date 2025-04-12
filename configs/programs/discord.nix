da3ba764ab1bf929f20bb87009e671811d2be5ee
ingenarel <ingenarelitems@gmail.com>
2025-04-12 16:53:51 +0600

    (fix): 711d4046aec6

--- configs/programs/discord.nix
+++ configs/programs/discord.nix
@@ -1,50 +1,53 @@
 {inputs, ...}: {
-    imports = [
-        inputs.nixcord.homeManagerModules.nixcord
-    ];
-    home-manager.users.ingenarel.programs.nixcord = {
-        discord = {
-            enable = false;
-            vencord.enable = false;
-            openASAR.enable = false;
-        };
-        vesktop.enable = true;
-        config = {
-            frameless = true;
-            transparent = true;
-            disableMinSize = true;
-            plugins = {
-                anonymiseFileNames.enable = true;
-                betterFolders = {
-                    enable = true;
-                    sidebarAnim = false;
-                    closeAllFolders = true;
-                    closeAllHomeButton = true;
-                    closeOthers = true;
-                    forceOpen = true;
+    home-manager.users.ingenarel = {
+        imports = [
+            inputs.nixcord.homeManagerModules.nixcord
+        ];
+        programs.nixcord = {
+            enable = true;
+            discord = {
+                enable = false;
+                vencord.enable = false;
+                openASAR.enable = false;
+            };
+            vesktop.enable = true;
+            config = {
+                frameless = true;
+                transparent = true;
+                disableMinSize = true;
+                plugins = {
+                    anonymiseFileNames.enable = true;
+                    betterFolders = {
+                        enable = true;
+                        sidebarAnim = false;
+                        closeAllFolders = true;
+                        closeAllHomeButton = true;
+                        closeOthers = true;
+                        forceOpen = true;
+                    };
+                    callTimer.enable = true;
+                    clearURLs.enable = true;
+                    crashHandler.enable = true;
+                    fakeNitro.enable = true;
+                    iLoveSpam.enable = true;
+                    imageLink.enable = true;
+                    imageZoom.enable = true;
+                    memberCount.enable = true;
+                    messageLatency.enable = true;
+                    messageLinkEmbeds.enable = true;
+                    messageLogger.enable = true;
+                    moreUserTags.enable = true;
+                    noBlockedMessages.enable = true;
+                    noTypingAnimation.enable = true;
+                    noUnblockToJump.enable = true;
+                    relationshipNotifier.enable = true;
+                    typingTweaks.enable = true;
+                    unindent.enable = true;
+                    voiceDownload.enable = true;
+                    voiceMessages.enable = true;
+                    webScreenShareFixes.enable = true;
+                    whoReacted.enable = true;
                 };
-                callTimer.enable = true;
-                clearURLs.enable = true;
-                crashHandler.enable = true;
-                fakeNitro.enable = true;
-                iLoveSpam.enable = true;
-                imageLink.enable = true;
-                imageZoom.enable = true;
-                memberCount.enable = true;
-                messageLatency.enable = true;
-                messageLinkEmbeds.enable = true;
-                messageLogger.enable = true;
-                moreUserTags.enable = true;
-                noBlockedMessages.enable = true;
-                noTypingAnimation.enable = true;
-                noUnblockToJump.enable = true;
-                relationshipNotifier.enable = true;
-                typingTweaks.enable = true;
-                unindent.enable = true;
-                voiceDownload.enable = true;
-                voiceMessages.enable = true;
-                webScreenShareFixes.enable = true;
-                whoReacted.enable = true;
             };
         };
     };
