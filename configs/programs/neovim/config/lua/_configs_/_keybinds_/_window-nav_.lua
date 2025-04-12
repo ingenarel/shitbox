c2d1abfa8f96f17c523d56b76f52af2adcd58073
ingenarel <ingenarelitems@gmail.com>
2025-04-13 05:54:33 +0600

    (neovim): change more :x<CR> to <CMD>x<CR>

--- configs/programs/neovim/config/lua/_configs_/_keybinds_/_window-nav_.lua
+++ configs/programs/neovim/config/lua/_configs_/_keybinds_/_window-nav_.lua
@@ -35,31 +35,32 @@ function CK_ChangeWindowAndShowHelp(grepvalue, splitdirection)
     if current_file == "help" then
         if grepvalue == true then
             CK_SimKeys(
-                "<C-w>" .. splitdirection .. "<C-w>w:helpg " .. callHelp .. "\\c<CR>:Telescope quickfix<CR>",
+                "<C-w>" .. splitdirection .. "<C-w>w<CMD>helpg " .. callHelp .. "\\c<CR><CMD>Telescope quickfix<CR>",
                 "n"
             )
         else
-            CK_SimKeys("<C-w>" .. splitdirection .. "<C-w>w:help " .. callHelp .. "<CR>", "n")
+            CK_SimKeys("<C-w>" .. splitdirection .. "<C-w>w<CMD>help " .. callHelp .. "<CR>", "n")
         end
     else
         if grepvalue == true then
-            CK_SimKeys(":helpg " .. callHelp .. "\\c<CR>:Telescope quickfix<CR>", "n")
+            CK_SimKeys("<CMD>helpg " .. callHelp .. "\\c<CR><CMD>Telescope quickfix<CR>", "n")
         else
-            CK_SimKeys(":help " .. callHelp .. "<CR>", "n")
+            CK_SimKeys("<CMD>help " .. callHelp .. "<CR>", "n")
         end
     end
 end -- }}}
 
 -- splits{{{
-local yazi = "<C-w>w:FloatermNew --width=0.95 --height=1.0 --title=YAZI --titleposition=center --opener=edit yazi<CR>"
+local yazi =
+    "<C-w>w<CMD>FloatermNew --width=0.95 --height=1.0 --title=YAZI --titleposition=center --opener=edit yazi<CR>"
 -- x direction
 CK_SetKeymap_n("<leader>wxx", "<C-w>v<C-w>w", "⫸ ")
-CK_SetKeymap_n("<leader>wxf", "<C-w>v<C-w>w:Telescope find_files<CR>", "⫸ files")
+CK_SetKeymap_n("<leader>wxf", "<C-w>v<C-w>w<CMD>Telescope find_files<CR>", "⫸ files")
 CK_SetKeymap_n("<leader>wxF", "<C-w>v" .. yazi, "⫸ yazi")
-CK_SetKeymap_n("<leader>wxo", "<C-w>v<C-w>w:Telescope oldfiles<CR>", "⫸ oldfiles")
-CK_SetKeymap_n("<leader>wxG", "<C-w>v<C-w>w:Telescope git_files<CR>", "⫸ gitfiles")
-CK_SetKeymap_n("<leader>wxg", "<C-w>v<C-w>w:Telescope live_grep<CR>", "⫸ grep")
-CK_SetKeymap_n("<leader>wxs", "<C-w>v<C-w>w:Telescope grep_string<CR>", "⫸ search current string")
+CK_SetKeymap_n("<leader>wxo", "<C-w>v<C-w>w<CMD>Telescope oldfiles<CR>", "⫸ oldfiles")
+CK_SetKeymap_n("<leader>wxG", "<C-w>v<C-w>w<CMD>Telescope git_files<CR>", "⫸ gitfiles")
+CK_SetKeymap_n("<leader>wxg", "<C-w>v<C-w>w<CMD>Telescope live_grep<CR>", "⫸ grep")
+CK_SetKeymap_n("<leader>wxs", "<C-w>v<C-w>w<CMD>Telescope grep_string<CR>", "⫸ search current string")
 CK_SetKeymap_n("<leader>wx]", "<C-w>v<C-w>w<C-]>", "⫸ opens up the tagfile/definition")
 CK_SetKeymap_n("<leader>wx<leader>", "<C-w>v<C-w>wgf", "⫸ opens up the file")
 -- vim.keymap.set("n", "<leader>wxh", function () CK_ChangeWindowAndShowHelp(false, 'v') end, {desc="⫸ help"})
@@ -72,12 +73,12 @@ end, { desc = "⫸ help grep" })
 -- x direction
 -- y direction
 CK_SetKeymap_n("<leader>wyy", "<C-w>s<C-w>w", "ↆ ")
-CK_SetKeymap_n("<leader>wyf", "<C-w>s<C-w>w:Telescope find_files<CR>", "ↆ files")
+CK_SetKeymap_n("<leader>wyf", "<C-w>s<C-w>w<CMD>Telescope find_files<CR>", "ↆ files")
 CK_SetKeymap_n("<leader>wyF", "<C-w>s" .. yazi, "ↆ yazi")
-CK_SetKeymap_n("<leader>wyo", "<C-w>s<C-w>w:Telescope oldfiles<CR>", "ↆ oldfiles")
-CK_SetKeymap_n("<leader>wyG", "<C-w>s<C-w>w:Telescope git_files<CR>", "ↆ gitfiles")
-CK_SetKeymap_n("<leader>wyg", "<C-w>s<C-w>w:Telescope live_grep<CR>", "ↆ grep")
-CK_SetKeymap_n("<leader>wys", "<C-w>s<C-w>w:Telescope grep_string<CR>", "ↆ search current string")
+CK_SetKeymap_n("<leader>wyo", "<C-w>s<C-w>w<CMD>Telescope oldfiles<CR>", "ↆ oldfiles")
+CK_SetKeymap_n("<leader>wyG", "<C-w>s<C-w>w<CMD>Telescope git_files<CR>", "ↆ gitfiles")
+CK_SetKeymap_n("<leader>wyg", "<C-w>s<C-w>w<CMD>Telescope live_grep<CR>", "ↆ grep")
+CK_SetKeymap_n("<leader>wys", "<C-w>s<C-w>w<CMD>Telescope grep_string<CR>", "ↆ search current string")
 CK_SetKeymap_n("<leader>wy]", "<C-w>s<C-w>w<C-]>", "ↆ opens up the tagfile/definition")
 CK_SetKeymap_n("<leader>wy<leader>", "<C-w>s<C-w>wgf", "⫸ opens up the file")
 vim.keymap.set("n", "<leader>wyh", function()
