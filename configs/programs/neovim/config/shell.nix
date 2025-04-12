4e0c6db1946a321da5bd573444930749c8b0f282
ingenarel <ingenarelitems@gmail.com>
2025-04-13 04:47:12 +0600

    (feat): added shell.nix for neovim

---
+++ configs/programs/neovim/config/shell.nix
@@ -0,0 +1,25 @@
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell {
+    shellHook = ''
+        export XDG_CONFIG_HOME=$HOME/.config/nixos-config/configs/programs/neovim
+        export NVIM_APPNAME=config
+        export DATA_SYMLINK="$HOME/.local/share/$NVIM_APPNAME"
+        export STATE_SYMLINK="$HOME/.local/state/$NVIM_APPNAME"
+        export CACHE_SYMLINK="$HOME/.cache/$NVIM_APPNAME"
+
+        [ -L "$DATA_SYMLINK" ] || ln -s "$HOME/.local/share/nvim" "$DATA_SYMLINK"
+        [ -L "$STATE_SYMLINK" ] || ln -s "$HOME/.local/state/nvim" "$STATE_SYMLINK"
+        [ -L "$CACHE_SYMLINK" ] || ln -s "$HOME/.cache/nvim" "$CACHE_SYMLINK"
+        echo "created symlinks"
+
+        cleanshit() {
+            [ -L "$DATA_SYMLINK" ] && rm "$DATA_SYMLINK"
+            [ -L "$STATE_SYMLINK" ] && rm "$STATE_SYMLINK"
+            [ -L "$CACHE_SYMLINK" ] && rm "$CACHE_SYMLINK"
+        }
+
+        trap cleanshit EXIT
+
+        nvim
+    '';
+}
