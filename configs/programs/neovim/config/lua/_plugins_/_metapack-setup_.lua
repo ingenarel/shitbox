12666c0720105839b3758498a9ada02ccc538626
ingenarel <ingenarelitems@gmail.com>
2025-04-13 20:23:16 +0600

    (metapack): change config according to the plugin change

--- configs/programs/neovim/config/lua/_plugins_/_metapack-setup_.lua
+++ configs/programs/neovim/config/lua/_plugins_/_metapack-setup_.lua
@@ -8,30 +8,32 @@ return {
         },
     },
     config = function()
-        require("metapack").ensure_installed {
-            --lsp
-            "pyright",
-            "clangd",
-            "lua-language-server",
-            "bash-language-server",
-            "termux-language-server",
-            "ltex-ls-plus",
-            "yaml-language-server",
-            "rust-analyzer",
-            --lsp
-            --dap
-            "codelldb",
-            "debugpy",
-            "bash-debug-adapter",
-            --dap
-            --formatter
-            "black",
-            "stylua",
-            "clang-format",
-            "beautysh",
-            "nil",
-            "alejandra",
-            --formatter
+        require("metapack").setup {
+            ensure_installed = {
+                --lsp
+                "pyright",
+                "clangd",
+                "lua-language-server",
+                "bash-language-server",
+                "termux-language-server",
+                "ltex-ls-plus",
+                "yaml-language-server",
+                "rust-analyzer",
+                --lsp
+                --dap
+                "codelldb",
+                "debugpy",
+                "bash-debug-adapter",
+                --dap
+                --formatter
+                "black",
+                "stylua",
+                "clang-format",
+                "beautysh",
+                "nil",
+                "alejandra",
+                --formatter
+            },
         }
     end,
 }
