a3943a78c8ff1dc934768369870ca913665592e2
ingenarel <ingenarelitems@gmail.com>
2025-04-20 01:19:22 +0600

    (nvim): specify width for terminal

--- configs/programs/neovim/nvim/lua/plugins/smart-floatterm/keybinds.lua
+++ configs/programs/neovim/nvim/lua/plugins/smart-floatterm/keybinds.lua
@@ -2,7 +2,7 @@ require("which-key").add {
     {
         "<leader><leader>t",
         function()
-            require("smart-floatterm").open()
+            require("smart-floatterm").open { widthPercentage = 90 }
         end,
         desc = "Terminal",
     },
