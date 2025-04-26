93508a0dcfe8ee20a8d25d529b4833a3d4e991ab
ingenarel <ingenarelitems@gmail.com>
2025-04-26 18:00:47 +0600

    (feat): make mpd use home-manager

--- configs/programs/mpd.nix
+++ configs/programs/mpd.nix
@@ -1,27 +1,22 @@
-{pkgs, ...}: {
-    environment.systemPackages = [pkgs.mpd];
-    services.mpd = {
+{...}: {
+    home-manager.users.ingenarel.services.mpd = {
         enable = true;
         musicDirectory = "/home/ingenarel/Music";
-        user = "ingenarel";
         extraConfig = ''
-          db_file            "/home/ingenarel/Music/mpd/database"
+            db_file            "/home/ingenarel/Music/mpd/database"
 
-          # Uncomment to refresh the database whenever files in the music_directory are changed
-          auto_update "yes"
+            # Uncomment to refresh the database whenever files in the music_directory are changed
+            auto_update "yes"
 
-          # Uncomment to enable the functionalities
-          playlist_directory "/home/ingenarel/Music/mpd/playlists"
-          pid_file           "/home/ingenarel/Music/mpd/pid"
-          state_file         "/home/ingenarel/Music/state"
-          sticker_file       "/home/ingenarel/Music/mpd/sticker.sql"
-          audio_output {
-              type "pipewire"
-              name "My PipeWire Output"
-          }
+            # Uncomment to enable the functionalities
+            playlist_directory "/home/ingenarel/Music/mpd/playlists"
+            pid_file           "/home/ingenarel/Music/mpd/pid"
+            state_file         "/home/ingenarel/Music/state"
+            sticker_file       "/home/ingenarel/Music/mpd/sticker.sql"
+            audio_output {
+                type "pipewire"
+                name "My PipeWire Output"
+            }
         '';
-    };
-    systemd.services.mpd.environment = {
-        XDG_RUNTIME_DIR = "/run/user/1000";
     };
 }
