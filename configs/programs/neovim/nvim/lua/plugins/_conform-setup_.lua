fc2093feba29516f1354593286ff659418224188
ingenarel <ingenarelitems@gmail.com>
2025-05-02 09:19:25 +0600

    (nvim): use a function for conform setup

--- configs/programs/neovim/nvim/lua/plugins/_conform-setup_.lua
+++ configs/programs/neovim/nvim/lua/plugins/_conform-setup_.lua
@@ -1,22 +1,25 @@
 return {
     "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            lua = { "stylua" },
-            python = { "black" },
-            c = { "clang-format" },
-            bash = { "beautysh" },
-            csh = { "beautysh" },
-            ksh = { "beautysh" },
-            zsh = { "beautysh" },
-            sh = { "beautysh" },
-            nix = { "alejandra" },
-        },
-        format_on_save = function(bufnr)
-            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-            end
-            return { timeout_ms = 500, lsp_format = "fallback" }
-        end,
-    },
+    config = function()
+        require("conform").setup {
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "black" },
+                c = { "clang-format" },
+                bash = { "beautysh" },
+                csh = { "beautysh" },
+                ksh = { "beautysh" },
+                zsh = { "beautysh" },
+                sh = { "beautysh" },
+                nix = { "alejandra" },
+            },
+        }
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function(args)
+                require("conform").format { bufnr = args.buf }
+            end,
+        })
+    end,
 }
