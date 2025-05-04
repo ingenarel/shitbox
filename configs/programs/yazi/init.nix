4a5b81cd500ff45d84f7b1a68130f1dd40b08922
ingenarel <ingenarelitems@gmail.com>
2025-05-04 10:13:35 +0600

    (yazi): add some yazi plugins

--- configs/programs/yazi/init.nix
+++ configs/programs/yazi/init.nix
@@ -11,5 +11,8 @@
         ".config/yazi/init.lua".source = ./config/init.lua;
         ".config/yazi/keymap.toml".source = ./config/keymap.toml;
         ".config/yazi/yazi.toml".source = ./config/yazi.toml;
+        ".config/yazi/plugins/exifaudio.yazi".source = ./config/plugins/exifaudio.yazi;
+        ".config/yazi/plugins/relative-motions.yazi".source = ./config/plugins/relative-motions.yazi;
+        ".config/yazi/plugins/starship.yazi".source = ./config/plugins/starship.yazi;
     };
 }
