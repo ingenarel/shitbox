df565ce8caa29652f2f790dfb863f5dd6654ccf5
ingenarel <ingenarelitems@gmail.com>
2025-05-13 21:30:47 +0000

    (nvim): if there's already a gitlab mirror, don't add one

--- configs/programs/neovim/nvim/develop-plugins.sh
+++ configs/programs/neovim/nvim/develop-plugins.sh
@@ -1,30 +1,36 @@
 #!/usr/bin/env bash
 
+addGitlabURL(){
+    if ! grep -qE 'url = git@gitlab.com:ingenarel' "$1"; then
+        sed -i -E 's/url = git@github.com:ingenarel\/(.+)/&\n\turl = git@gitlab.com:ingenarel\/\1/' "$1"
+    fi
+}
+
 if [[ ! -d "$HOME/coding/git" ]]; then
     mkdir --parents "$HOME/coding/git"
 fi
 
 git clone git@github.com:ingenarel/metapack.nvim "$HOME/coding/git/metapack.nvim"
 git -C "$HOME/coding/git/metapack.nvim" switch dev
-sed -E 's/url = git@github.com:ingenarel\/(.+)/&\n\turl = git@gitlab.com:ingenarel\/\1/' "$HOME/coding/git/metapack.nvim/.git/config"
+addGitlabURL "$HOME/coding/git/metapack.nvim/.git/config"
 
 git clone git@github.com:ingenarel/luix.nvim "$HOME/coding/git/luix.nvim"
-sed -E 's/url = git@github.com:ingenarel\/(.+)/&\n\turl = git@gitlab.com:ingenarel\/\1/' "$HOME/coding/git/luix.nvim/.git/config"
+addGitlabURL "$HOME/coding/git/luix.nvim/.git/config"
 
 git clone git@github.com:ingenarel/smart-term.nvim "$HOME/coding/git/smart-term.nvim"
-sed -E 's/url = git@github.com:ingenarel\/(.+)/&\n\turl = git@gitlab.com:ingenarel\/\1/'  "$HOME/coding/git/smart-term.nvim/.git/config"
+addGitlabURL  "$HOME/coding/git/smart-term.nvim/.git/config"
 
 git clone git@github.com:ingenarel/simple-runner.nvim "$HOME/coding/git/simple-runner.nvim"
-sed -E 's/url = git@github.com:ingenarel\/(.+)/&\n\turl = git@gitlab.com:ingenarel\/\1/'  "$HOME/coding/git/simple-runner.nvim/.git/config"
+addGitlabURL  "$HOME/coding/git/simple-runner.nvim/.git/config"
 
 git clone git@github.com:ingenarel/focus-mode.nvim "$HOME/coding/git/focus-mode.nvim"
-sed -E 's/url = git@github.com:ingenarel\/(.+)/&\n\turl = git@gitlab.com:ingenarel\/\1/' "$HOME/coding/git/focus-mode.nvim/.git/config"
+addGitlabURL "$HOME/coding/git/focus-mode.nvim/.git/config"
 
 git clone git@github.com:ingenarel/cyberpunk-neon.nvim "$HOME/coding/git/cyberpunk-neon.nvim"
-sed -E 's/url = git@github.com:ingenarel\/(.+)/&\n\turl = git@gitlab.com:ingenarel\/\1/' "$HOME/coding/git/cyberpunk-neon.nvim/.git/config"
+addGitlabURL "$HOME/coding/git/cyberpunk-neon.nvim/.git/config"
 
 git clone git@github.com:ingenarel/randomtips-nvim "$HOME/coding/git/randomtips-nvim"
-sed -E 's/url = git@github.com:ingenarel\/(.+)/&\n\turl = git@gitlab.com:ingenarel\/\1/' "$HOME/coding/git/randomtips-nvim/.git/config"
+addGitlabURL "$HOME/coding/git/randomtips-nvim/.git/config"
 
 git clone git@github.com:ingenarel/nvim-pairMan "$HOME/coding/git/nvim-pairMan"
-sed -E 's/url = git@github.com:ingenarel\/(.+)/&\n\turl = git@gitlab.com:ingenarel\/\1/' "$HOME/coding/git/nvim-pairMan/.git/config"
+addGitlabURL "$HOME/coding/git/nvim-pairMan/.git/config"
