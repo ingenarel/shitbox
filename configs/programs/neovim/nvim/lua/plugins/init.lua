0b203101b0d28981f8575f3ba07bd484ae41a9a0
ingenarel <ingenarelitems@gmail.com>
2025-05-14 21:10:55 +0600

    (nvim): update plugin as per the new config

--- configs/programs/neovim/nvim/lua/plugins/init.lua
+++ configs/programs/neovim/nvim/lua/plugins/init.lua
@@ -48,7 +48,7 @@ require("lazy").setup {
                 version = "*",
                 config = { keys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" },
             },
-            { "m4xshen/hardtime.nvim", dependencies = { "MunifTanjim/nui.nvim" }, opts = { disabled_keys = "" } },
+            { "m4xshen/hardtime.nvim", dependencies = { "MunifTanjim/nui.nvim" }, opts = { disabled_keys = {} } },
             { "folke/persistence.nvim", event = "BufReadPre", opts = {} },
             { "imsnif/kdl.vim", ft = "kdl" },
             { "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" }, opts = {} },
