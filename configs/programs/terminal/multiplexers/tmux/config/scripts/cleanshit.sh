881e7338c2ee8d50f0ca71533baaa510f1e1d931
ingenarel <ingenarelitems@gmail.com>
2025-05-18 01:12:09 +0600

    (cleanscript): added trash

--- configs/programs/terminal/multiplexers/tmux/config/scripts/cleanshit.sh
+++ configs/programs/terminal/multiplexers/tmux/config/scripts/cleanshit.sh
@@ -4,4 +4,4 @@ if grep -qEi "nixos" /etc/os-release ; then
     nix-collect-garbage -d
     sudo nix-collect-garbage -d
 fi
-sudo rm -rf "$HOME/.cache/*" "$HOME/.cargo" "$HOME/.npm"
+sudo rm -rf "$HOME/.cache/*" "$HOME/.cargo" "$HOME/.npm" "$HOME/.local/share/Trash"
