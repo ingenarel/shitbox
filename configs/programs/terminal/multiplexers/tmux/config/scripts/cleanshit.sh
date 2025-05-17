3890aa8d7271c908a324c790587b4a00f7e87dc2
ingenarel <ingenarelitems@gmail.com>
2025-05-17 14:18:09 +0600

    (script): add basic script to clean stuff

---
+++ configs/programs/terminal/multiplexers/tmux/config/scripts/cleanshit.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if grep -qEi "nixos" /etc/os-release ; then
+    nix-collect-garbage -d
+    sudo nix-collect-garbage -d
+fi
+sudo rm -rf "$HOME/.cache/*" "$HOME/.cargo" "$HOME/.npm"
