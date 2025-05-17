54fb906de4b71642a320b0f5cdadc4ac9f43053c
ingenarel <ingenarelitems@gmail.com>
2025-05-18 00:59:55 +0600

    (nvim): call smart-term one time

--- configs/programs/neovim/nvim/lua/plugins/smart-term/keybinds.lua
+++ configs/programs/neovim/nvim/lua/plugins/smart-term/keybinds.lua
@@ -1,15 +1,16 @@
+local term = require("smart-term")
 require("which-key").add {
     {
         "<leader><leader>t",
         function()
-            require("smart-term").openFloaTerm { widthPercentage = 90 }
+            term.openFloaTerm { widthPercentage = 90 }
         end,
         desc = "Terminal",
     },
     {
         "<leader>gg",
         function()
-            require("smart-term").openNeovimFloaTerm { widthPercentage = 90, heightPercentage = 90, command = "lazygit" }
+            term.openNeovimFloaTerm { widthPercentage = 90, heightPercentage = 90, command = "lazygit" }
         end,
         desc = "Lazygit",
     },
