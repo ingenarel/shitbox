a7a0c2174a7788d4c6d467856fccd32f8004e4f0
ingenarel <ingenarelitems@gmail.com>
2025-05-21 05:19:11 +0600

    (scripts): add gentoo stuff to cleanscript

--- configs/programs/terminal/multiplexers/tmux/config/scripts/cleanshit.sh
+++ configs/programs/terminal/multiplexers/tmux/config/scripts/cleanshit.sh
@@ -3,5 +3,8 @@
 if grep -qEi "nixos" /etc/os-release ; then
     nix-collect-garbage -d
     sudo nix-collect-garbage -d
+elif grep -qEi "gentoo" /etc/os-release ; then
+    sudo emerge --deep --depclean
+    sudo rm -rf /var/cache/binpkgs/* /var/cache/distfiles/*
 fi
 sudo rm -rf "$HOME/.cache/*" "$HOME/.cargo" "$HOME/.npm" "$HOME/.local/share/Trash"
