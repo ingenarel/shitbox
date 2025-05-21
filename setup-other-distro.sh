370238b912de7d29814b3b3de92f5072c1d88c14
ingenarel <ingenarelitems@gmail.com>
2025-05-21 08:14:01 +0600

    (scripts): fix setting up yazi properly

--- setup-other-distro.sh
+++ setup-other-distro.sh
@@ -9,6 +9,14 @@ setupConfigs(){
     ln -sf "$scriptDir/configs/programs/zsh/config"                                         "$HOME/.config/zsh"
     ln -sf "$scriptDir/configs/programs/git/config"                                         "$HOME/.gitconfig"
     echo "source $HOME/.config/zsh/init.zsh" >                                              "$HOME/.zshrc"
+    ln -sf "$scriptDir/configs/programs/yazi/config/alias"                                  "$HOME/.config/yazi/alias"
+    ln -sf "$scriptDir/configs/programs/yazi/config/init.lua"                               "$HOME/.config/yazi/init.lua"
+    ln -sf "$scriptDir/configs/programs/yazi/config/keymap.toml"                            "$HOME/.config/yazi/keymap.toml"
+    ln -sf "$scriptDir/configs/programs/yazi/config/yazi.toml"                              "$HOME/.config/yazi/yazi.toml"
+    ln -sf "$scriptDir/configs/programs/yazi/config/plugins/exifaudio.yazi"                 "$HOME/.config/yazi/plugins/exifaudio.yazi"
+    ln -sf "$scriptDir/configs/programs/yazi/config/plugins/relative-motions.yazi"          "$HOME/.config/yazi/plugins/relative-motions.yazi"
+    ln -sf "$scriptDir/configs/programs/yazi/config/plugins/starship.yazi"                  "$HOME/.config/yazi/plugins/starship.yazi"
+    ln -sf "$scriptDir/configs/programs/yazi/config/plugins/plugins/git.yazi"               "$HOME/.config/yazi/plugins/git.yazi"
 }
 
 setupConfigs
