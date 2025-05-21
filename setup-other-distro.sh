6a5ab3280c9c37813a3580b4e58adaadec1f5a94
ingenarel <ingenarelitems@gmail.com>
2025-05-21 08:40:28 +0600

    (scripts): use a safe linking function for the other distro script

--- setup-other-distro.sh
+++ setup-other-distro.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 scriptDir="$(realpath --canonicalize-missing "${BASH_SOURCE[0]}/..")"
 
+safelink(){
+    [[ -d "$(realpath --canonicalize-missing "$2/..")" ]] || mkdir --parents "$2"
+    [[ -d "$2" ]] || ln -sf "$1" "$2"
+}
+
 setupConfigs(){
-    ln -sf "$scriptDir/configs/programs/neovim/nvim"                                        "$HOME/.config/nvim"
-    ln -sf "$scriptDir/configs/programs/lazygit/config"                                     "$HOME/.config/lazygit"
-    ln -sf "$scriptDir/configs/programs/terminal/multiplexers/tmux/config"                  "$HOME/.config/tmux"
-    ln -sf "$scriptDir/configs/programs/terminal/multiplexers/tmux/tmux-powerline-config"   "$HOME/.config/tmux-powerline"
-    ln -sf "$scriptDir/configs/programs/zsh/config"                                         "$HOME/.config/zsh"
-    ln -sf "$scriptDir/configs/programs/git/config"                                         "$HOME/.gitconfig"
-    echo "source $HOME/.config/zsh/init.zsh" >                                              "$HOME/.zshrc"
-    ln -sf "$scriptDir/configs/programs/yazi/config/alias"                                  "$HOME/.config/yazi/alias"
-    ln -sf "$scriptDir/configs/programs/yazi/config/init.lua"                               "$HOME/.config/yazi/init.lua"
-    ln -sf "$scriptDir/configs/programs/yazi/config/keymap.toml"                            "$HOME/.config/yazi/keymap.toml"
-    ln -sf "$scriptDir/configs/programs/yazi/config/yazi.toml"                              "$HOME/.config/yazi/yazi.toml"
-    ln -sf "$scriptDir/configs/programs/yazi/config/plugins/exifaudio.yazi"                 "$HOME/.config/yazi/plugins/exifaudio.yazi"
-    ln -sf "$scriptDir/configs/programs/yazi/config/plugins/relative-motions.yazi"          "$HOME/.config/yazi/plugins/relative-motions.yazi"
-    ln -sf "$scriptDir/configs/programs/yazi/config/plugins/starship.yazi"                  "$HOME/.config/yazi/plugins/starship.yazi"
-    ln -sf "$scriptDir/configs/programs/yazi/config/plugins/plugins/git.yazi"               "$HOME/.config/yazi/plugins/git.yazi"
+    safelink "$scriptDir/configs/programs/neovim/nvim"                                          "$HOME/.config/nvim"
+    safelink "$scriptDir/configs/programs/lazygit/config"                                       "$HOME/.config/lazygit"
+    safelink "$scriptDir/configs/programs/terminal/multiplexers/tmux/config"                    "$HOME/.config/tmux"
+    safelink "$scriptDir/configs/programs/terminal/multiplexers/tmux/tmux-powerline-config"     "$HOME/.config/tmux-powerline"
+    safelink "$scriptDir/configs/programs/zsh/config"                                           "$HOME/.config/zsh"
+    safelink "$scriptDir/configs/programs/git/config"                                           "$HOME/.gitconfig"
+    echo "source $HOME/.config/zsh/init.zsh" >                                                  "$HOME/.zshrc"
+    safelink "$scriptDir/configs/programs/yazi/config/alias"                                    "$HOME/.config/yazi/alias"
+    safelink "$scriptDir/configs/programs/yazi/config/init.lua"                                 "$HOME/.config/yazi/init.lua"
+    safelink "$scriptDir/configs/programs/yazi/config/keymap.toml"                              "$HOME/.config/yazi/keymap.toml"
+    safelink "$scriptDir/configs/programs/yazi/config/yazi.toml"                                "$HOME/.config/yazi/yazi.toml"
+    safelink "$scriptDir/configs/programs/yazi/config/plugins/exifaudio.yazi"                   "$HOME/.config/yazi/plugins/exifaudio.yazi"
+    safelink "$scriptDir/configs/programs/yazi/config/plugins/relative-motions.yazi"            "$HOME/.config/yazi/plugins/relative-motions.yazi"
+    safelink "$scriptDir/configs/programs/yazi/config/plugins/starship.yazi"                    "$HOME/.config/yazi/plugins/starship.yazi"
+    safelink "$scriptDir/configs/programs/yazi/config/plugins/plugins/git.yazi"                 "$HOME/.config/yazi/plugins/git.yazi"
 }
 
 setupConfigs
