174a6d0c37187d79894c29e3f5286b2a70b12873
ingenarel <ingenarelitems@gmail.com>
2025-05-23 04:39:56 +0600

    (fix): test /run/archiso as a dir

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 scriptDir="$(realpath --canonicalize-missing "${BASH_SOURCE[0]}/..")"
 
-[[ -f /run/archiso ]] && "$scriptDir/install/arch-pre.sh" "$1" "$2" "$3"
+[[ -d /run/archiso ]] && "$scriptDir/install/arch-pre.sh" "$1" "$2" "$3"
 "$scriptDir/configs/setup.sh"
