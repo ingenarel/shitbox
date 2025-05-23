84db4120b2e41b0c9ac4791f70bae83fb83a5f95
ingenarel <ingenarelitems@gmail.com>
2025-05-23 08:31:10 +0600

    (feat): add echos to explain what the commands are doing

--- install/arch-pre.sh
+++ install/arch-pre.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 mbr(){
-    mkfs.ext4 "/dev/$1"
+    echo "Formatting /dev/$1 to ext4" && mkfs.ext4 "/dev/$1" && echo "Formatted /dev/$1 to ext4"
 }
 
 [[ -z $3 || $3 == "mbr" ]] && mbr "$2"
-mount "/dev/$2" /mnt
+
+echo "Mounted /dev/$2 to /mnt" && mount "/dev/$2" /mnt && echo "Mounted /dev/$2 to /mnt"
 
 pacstrapCommand="pacstrap -K /mnt\
     base\
@@ -19,14 +20,15 @@ pacstrapCommand="pacstrap -K /mnt\
     sudo"
 
 dmidecode -s system-manufacturer | grep -qEi 'qemu' || pacstrapCommand="${pacstrapCommand}\linux-firmware intel-ucode"
-eval "$pacstrapCommand"
+echo "Running pacstrap" && eval "$pacstrapCommand" && echo "ran pacstrap"
 
-genfstab -U /mnt >> /mnt/etc/fstab
+echo "Generating fstab" && genfstab -U /mnt >> /mnt/etc/fstab && echo "Generated fstab"
 archPostPath="/tmp/arch-post.sh"
 arch-chroot /mnt "/usr/bin/bash"\
     "-c"\
-    "useradd --groups wheel --create-home ingenarel;\
-    curl https://raw.githubusercontent.com/ingenarel/shitbox/refs/heads/master/install/arch-post.sh > $archPostPath;\
-    chmod u+x $archPostPath;\
-    $archPostPath $1 mbr;\
+    "\
+    echo 'Creating user' && useradd --groups wheel --create-home ingenarel && echo 'Created user';\
+    echo 'Installing post-install script' && curl https://raw.githubusercontent.com/ingenarel/shitbox/refs/heads/master/install/arch-post.sh > $archPostPath echo 'Installed post-chroot script';\
+    echo 'Making script an executable' && chmod u+x $archPostPath && echo 'Made script an executable';\
+    echo 'Executing post-install script' $archPostPath $1 mbr && echo 'Executed post-install script';\
     "
