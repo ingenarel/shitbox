ee3768110c6290c21d4e5d4e4babd3e32e82d3b9
ingenarel <ingenarelitems@gmail.com>
2025-05-24 14:14:00 +0600

    (feat): add safelink function to script

--- ensureInstalled/arch.sh
+++ ensureInstalled/arch.sh
@@ -9,6 +9,11 @@ ram="$((
         ) / (1000 * 1000)
 ))"
 
+safelink(){
+    [[ -d "$(realpath --canonicalize-missing "$2/..")" ]] || ([[ "$3" != 1 ]] && mkdir --parents "$2" || sudo mkdir --parents "$2")
+    [[ -d "$2" ]] || ([[ "$3" != 1 ]] && ln -sf "$1" "$2" || sudo ln -sf "$1" "$2" && echo "linked $1 to $2")
+}
+
 packagesToInstall=""
 
 for package in "${packages[@]}"; do
