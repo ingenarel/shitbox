9e7ed8e0a7e645cfca756e4aadf9ea37fa7224d7
ingenarel <ingenarelitems@gmail.com>
2025-05-25 01:52:43 +0600

    (fix): safelink function

--- ensureInstalled/arch.sh
+++ ensureInstalled/arch.sh
@@ -10,7 +10,8 @@ ram="$((
 ))"
 
 safelink(){
-    [[ -d "$(realpath --canonicalize-missing "$2/..")" ]] || ([[ "$3" != 1 ]] && mkdir --parents "$2" || sudo mkdir --parents "$2")
+    parentPath="$(realpath --canonicalize-missing "$2/..")"
+    [[ -d "$parentPath" ]] || ([[ "$3" != 1 ]] && mkdir --parents "$parentPath" || sudo mkdir --parents "$parentPath")
     [[ -d "$2" ]] || ([[ "$3" != 1 ]] && ln -sf "$1" "$2" || sudo ln -sf "$1" "$2" && echo "linked $1 to $2")
 }
 
