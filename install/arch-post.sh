a7003cfcb0684b7979c1ff4feb8a6e69759f543e
ingenarel <ingenarelitems@gmail.com>
2025-05-25 04:16:47 +0600

    (feat): don't try to autoquit nvim

--- install/arch-post.sh
+++ install/arch-post.sh
@@ -36,4 +36,4 @@ echo "installed $aurHelper"
 "$shitboxDir/ensureInstalled/arch.sh"
 "$shitboxDir/configs/programs/neovim/nvim/develop-plugins.sh"
 command -v zsh && zsh -c "source ~/.zshrc"
-command -v nvim && nvim --headless --cmd ':q'
+command -v nvim && nvim --headless
