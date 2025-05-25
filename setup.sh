65a28ad90d0153bfcb10ed5f358d4a958dc423e3
ingenarel <ingenarelitems@gmail.com>
2025-05-26 05:12:01 +0600

    (fix): path

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ scriptDir="$(realpath --canonicalize-missing "${BASH_SOURCE[0]}/..")"
 
 [[ -d /run/archiso ]] && "$scriptDir/install/arch-pre.sh" "$1" "$2" "$3"
 
-([[ $HOSTNAME == "livecd" ]] && grep -qEi "gentoo" /etc//etc/os-release) && "$scriptDir/install/gentoo-pre.sh" "$1" "$2" "$3"
+([[ $HOSTNAME == "livecd" ]] && grep -qEi "gentoo" /etc/os-release) && "$scriptDir/install/gentoo-pre.sh" "$1" "$2" "$3"
