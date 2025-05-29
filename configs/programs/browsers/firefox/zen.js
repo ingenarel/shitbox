6b81cc4e36c358640f5b50f039d0851e2e502c47
ingenarel <ingenarelitems@gmail.com>
2025-05-29 12:27:51 +0600

    (firefox): try to enable extensions on restricted sites

--- configs/programs/browsers/firefox/zen.js
+++ configs/programs/browsers/firefox/zen.js
@@ -1,3 +1,6 @@
 user_pref("browser.compactmode.show", true);
 user_pref("zen.view.compact.hide-toolbar", true);
 user_pref("zen.welcome-screen.seen", true);
+
+user_pref("extensions.webextensions.restrictedDomains", "");
+user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
