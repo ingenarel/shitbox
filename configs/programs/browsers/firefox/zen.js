ecd140d8a11d5fcf99800b3fe790fac62a1d5210
ingenarel <ingenarelitems@gmail.com>
2025-05-29 16:59:53 +0600

    (zen): allow transparency

--- configs/programs/browsers/firefox/zen.js
+++ configs/programs/browsers/firefox/zen.js
@@ -2,5 +2,8 @@ user_pref("browser.compactmode.show", true);
 user_pref("zen.view.compact.hide-toolbar", true);
 user_pref("zen.welcome-screen.seen", true);
 
+user_pref("browser.tabs.allow_transparent_browser", true);
+user_pref("zen.widget.linux.transparency", true);
+
 user_pref("extensions.webextensions.restrictedDomains", "");
 user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
