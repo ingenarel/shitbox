12c25b76c96b4d37c6cf94c6e0ed27481242551a
ingenarel <ingenarelitems@gmail.com>
2025-05-29 17:00:01 +0600

    (firefox): update user.js

--- configs/programs/browsers/firefox/ingenarel/user.js
+++ configs/programs/browsers/firefox/ingenarel/user.js
@@ -1,3 +1,5 @@
+
+
 user_pref("app.normandy.api_url", "");
 user_pref("app.normandy.enabled", false);
 user_pref("app.shield.optoutstudies.enabled", false);
@@ -18,12 +20,16 @@ user_pref("browser.newtabpage.activity-stream.telemetry", false);
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.privatebrowsing.forceMediaMemoryCache", true); // [FF75+]
 user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.send_pings", false); // [DEFAULT: false]
 user_pref("browser.shell.shortcutFavicons", false);
 user_pref("browser.shopping.experience2023.enabled", false); // [DEFAULT: false]
+user_pref("browser.tabs.allow_transparent_browser", true);
 user_pref("browser.tabs.crashReporting.sendReport", false); // [FF44+]
 user_pref("browser.tabs.searchclipboardfor.middleclick", false); // [DEFAULT: false NON-LINUX]
 user_pref("browser.uitour.enabled", false);
+user_pref("browser.uitour.url", ""); // Defense-in-depth
 user_pref("browser.urlbar.addons.featureGate", false); // [FF115+]
+user_pref("browser.urlbar.clipboard.featureGate", false);
 user_pref("browser.urlbar.fakespot.featureGate", false); // [FF130+] [DEFAULT: false]
 user_pref("browser.urlbar.mdn.featureGate", false); // [FF117+] [HIDDEN PREF]
 user_pref("browser.urlbar.pocket.featureGate", false); // [FF116+] [DEFAULT: false]
@@ -39,20 +45,32 @@ user_pref("browser.urlbar.yelp.featureGate", false); // [FF124+]
 user_pref("browser.xul.error_pages.expert_bad_cert", true);
 user_pref("captivedetect.canonicalURL", "");
 user_pref("dom.disable_window_move_resize", true);
+user_pref("extensions.webextensions.restrictedDomains", "");
 user_pref("network.auth.subresource-http-auth-allow", 1);
 user_pref("network.captive-portal-service.enabled", false); // [FF52+]
+user_pref("network.cookie.cookieBehavior.optInPartitioning", true); // [ETP FF132+]
 user_pref("network.dns.disablePrefetchFromHTTPS", true);
 user_pref("network.dns.disablePrefetch", true);
 user_pref("network.file.disable_unc_paths", true); // [HIDDEN PREF]
 user_pref("network.gio.supported-protocols", ""); // [HIDDEN PREF] [DEFAULT: ""]
+user_pref("network.http.microsoft-entra-sso.enabled", false); // [DEFAULT: false]
+user_pref("network.http.referer.disallowCrossSiteRelaxingDefault.top_navigation", true); // [FF100+]
+user_pref("network.http.referer.disallowCrossSiteRelaxingDefault", true);
 user_pref("network.http.referer.spoofSource", false); // [DEFAULT: false]
 user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 user_pref("network.http.speculative-parallel-limit", 0);
+user_pref("network.http.windows-sso.enabled", false); // [DEFAULT: false]
 user_pref("network.IDN_show_punycode", true);
 user_pref("network.prefetch-next", false);
 user_pref("network.proxy.socks_remote_dns", true);
+user_pref("permissions.default.shortcuts", 1);
 user_pref("permissions.manager.defaultsUrl", "");
+user_pref("privacy.bounceTrackingProtection.mode", 1); // [FF131+] [ETP FF133+]
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.fingerprintingProtection", true); // [FF114+] [ETP FF119+]
 user_pref("privacy.firstparty.isolate", false); // [DEFAULT: false]
+user_pref("privacy.partition.network_state.ocsp_cache", true); // [DEFAULT: true]
+user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
 user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
 user_pref("privacy.sanitize.sanitizeOnShutdown", true);
 user_pref("privacy.sanitize.timeSpan", 0);
@@ -64,3 +82,4 @@ user_pref("toolkit.winRegisterApplicationRestart", false);
 user_pref("widget.non-native-theme.use-theme-accent", false); // [DEFAULT: false WINDOWS]
 user_pref("zen.view.compact.hide-toolbar", true);
 user_pref("zen.welcome-screen.seen", true);
+user_pref("zen.widget.linux.transparency", true);
