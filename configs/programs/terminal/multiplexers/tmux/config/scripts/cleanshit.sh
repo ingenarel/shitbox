b4bab89b9c6354487152d02e43d6496b6e714666
ingenarel <ingenarelitems@gmail.com>
2025-05-30 13:20:27 +0600

    (script): add arch entry for cleanscript

--- configs/programs/terminal/multiplexers/tmux/config/scripts/cleanshit.sh
+++ configs/programs/terminal/multiplexers/tmux/config/scripts/cleanshit.sh
@@ -6,5 +6,8 @@ if grep -qEi "nixos" /etc/os-release ; then
 elif grep -qEi "gentoo" /etc/os-release ; then
     sudo emerge --deep --depclean
     sudo rm -rf /var/cache/binpkgs/* /var/cache/distfiles/*
+elif grep -qEi "arch" /etc/os-release ; then
+    paru -Qtdq | paru -Rns -
+    paru -Sccd
 fi
 sudo rm -rf "$HOME/.cache/*" "$HOME/.cargo" "$HOME/.npm" "$HOME/.local/share/Trash"
