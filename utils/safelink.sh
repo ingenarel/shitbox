4dd7d1f707e8362e830f5d62250a44bdfe24112d
ingenarel <ingenarelitems@gmail.com>
2025-06-02 10:35:48 +0600

    (feat): do more line breaks in order to be more "readable"

--- utils/safelink.sh
+++ utils/safelink.sh
@@ -2,6 +2,27 @@
 
 safelink(){
     parentPath="$(realpath --canonicalize-missing "$2/..")"
-    [[ -d "$parentPath" ]] || ([[ "$3" != 1 ]] && mkdir --parents "$parentPath" || sudo mkdir --parents "$parentPath")
-    [[ -h "$2" && "$(realpath --canonicalize-missing "$2")" == "$1" ]] || ([[ "$3" != 1 ]] && ln -sf "$1" "$2" || sudo ln -sf "$1" "$2" && echo "linked $1 to $2")
+
+    [[ -d "$parentPath" ]]\
+        ||
+    (
+        [[ "$3" != 1 ]]\
+            &&
+        mkdir --parents "$parentPath"\
+            ||
+        sudo mkdir --parents "$parentPath"
+    )
+
+    [[ -h "$2" && "$(realpath --canonicalize-missing "$2")" == "$1" ]]\
+        ||
+    (
+        [[ "$3" != 1 ]]\
+            &&
+        ln -sf "$1" "$2"\
+            ||
+        sudo ln -sf "$1" "$2"\
+            &&
+        echo "linked $1 to $2"
+    )
+
 }
