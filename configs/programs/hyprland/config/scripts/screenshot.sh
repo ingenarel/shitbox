6ef6c9b5a0e73ca1b0553c230b16514241c4b23c
ingenarel <ingenarelitems@gmail.com>
2025-06-02 22:45:25 +0600

    (feat): change screenshot script to use satty

--- configs/programs/hyprland/config/scripts/screenshot.sh
+++ configs/programs/hyprland/config/scripts/screenshot.sh
@@ -1,7 +1,19 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-if command -v flameshot; then
-    flameshot gui
-else
-    grim -g "$(slurp)" - | wl-copy
-fi
+res="$(
+    for shit in /sys/class/drm/card*-*/; do
+        [[ -f "$shit/status" && "$(< "$shit/status")" == "connected" ]] && { head -n1 "$shit/modes" && break; }
+    done
+)"
+
+echo "$res"
+
+grim -g "0,0 $res" -t ppm -\
+    |
+satty\
+    --fullscreen\
+    --actions-on-escape save-to-clipboard,exit\
+    --copy-command wl-copy\
+    --disable-notifications\
+    --filename -\
+    --output-filename "$HOME/Pictures/$(date +%Y-%m-%d_%H-%m-%s).png"
