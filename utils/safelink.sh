4d496f7798ff413a86541276b3aaf33f277fdcc3
ingenarel <ingenarelitems@gmail.com>
2025-06-02 10:37:16 +0600

    (feat): use {...} instead of (...) in safelink

--- utils/safelink.sh
+++ utils/safelink.sh
@@ -5,24 +5,24 @@ safelink(){
 
     [[ -d "$parentPath" ]]\
         ||
-    (
+    {
         [[ "$3" != 1 ]]\
             &&
         mkdir --parents "$parentPath"\
             ||
-        sudo mkdir --parents "$parentPath"
-    )
+        sudo mkdir --parents "$parentPath";
+    }
 
     [[ -h "$2" && "$(realpath --canonicalize-missing "$2")" == "$1" ]]\
         ||
-    (
+    {
         [[ "$3" != 1 ]]\
             &&
         ln -sf "$1" "$2"\
             ||
         sudo ln -sf "$1" "$2"\
             &&
-        echo "linked $1 to $2"
-    )
+        echo "linked $1 to $2";
+    }
 
 }
