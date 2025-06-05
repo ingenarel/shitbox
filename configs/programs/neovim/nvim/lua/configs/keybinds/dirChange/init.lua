5f7d950251e2e6b09e3002f7905c2dc2057b0dea
ingenarel <ingenarelitems@gmail.com>
2025-06-05 19:14:11 +0600

    (nvim): add keymaps to change dir to neovim plugins

--- configs/programs/neovim/nvim/lua/configs/keybinds/dirChange/init.lua
+++ configs/programs/neovim/nvim/lua/configs/keybinds/dirChange/init.lua
@@ -6,9 +6,49 @@ vim.keymap.set("n", "<leader>cds", function()
     vim.cmd("cd ~/.config/shitbox | pwd ")
 end, { desc = "Changes directory to shitbox", silent = true, noremap = true })
 
-vim.keymap.set("n", "<leader>cdn", function()
+vim.keymap.set("n", "<leader>cdnn", function()
     vim.cmd("cd ~/.config/shitbox/configs/programs/neovim/nvim | pwd ")
 end, { desc = "Changes directory to nvim config", silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>cdnc", function()
+    vim.cmd("cd ~/.config/shitbox/configs/programs/neovim/myPlugins/cyberpunk-neon.nvim | pwd ")
+end, { desc = "Changes directory to cyberpunk-neon.nvim", silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>cdnf", function()
+    vim.cmd("cd ~/.config/shitbox/configs/programs/neovim/myPlugins/focus-mode.nvim | pwd ")
+end, { desc = "Changes directory to focus-mode.nvim", silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>cdnl", function()
+    vim.cmd("cd ~/.config/shitbox/configs/programs/neovim/myPlugins/luix.nvim | pwd ")
+end, { desc = "Changes directory to luix.nvim", silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>cdnL", function()
+    vim.cmd("cd ~/.config/shitbox/configs/programs/neovim/myPlugins/luse.nvim | pwd ")
+end, { desc = "Changes directory to luse.nvim", silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>cdnm", function()
+    vim.cmd("cd ~/.config/shitbox/configs/programs/neovim/myPlugins/metapack.nvim | pwd ")
+end, { desc = "Changes directory to metapack.nvim", silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>cdnp", function()
+    vim.cmd("cd ~/.config/shitbox/configs/programs/neovim/myPlugins/nvim-pairMan | pwd ")
+end, { desc = "Changes directory to nvim-pairMan", silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>cdnR", function()
+    vim.cmd("cd ~/.config/shitbox/configs/programs/neovim/myPlugins/randomtips-nvim | pwd ")
+end, { desc = "Changes directory to randomtips-nvim", silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>cdnz", function()
+    vim.cmd("cd ~/.config/shitbox/configs/programs/neovim/myPlugins/zoxide.nvim | pwd ")
+end, { desc = "Changes directory to zoxide.nvim", silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>cdnt", function()
+    vim.cmd("cd ~/.config/shitbox/configs/programs/neovim/myPlugins/smart-term.nvim | pwd ")
+end, { desc = "Changes directory to smart-term.nvim", silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>cdnr", function()
+    vim.cmd("cd ~/.config/shitbox/configs/programs/neovim/myPlugins/simple-runner.nvim | pwd ")
+end, { desc = "Changes directory to simple-runner.nvim", silent = true, noremap = true })
 
 vim.keymap.set("n", "<leader>cdh", function()
     vim.cmd("cd ~/.config/shitbox/configs/programs/hyprland/config | pwd ")
