f06b793a31a0c9ed275711d010cfe33d170d8cc1
ingenarel <ingenarelitems@gmail.com>
2025-06-25 00:08:17 +0600

    (feat): seperated fuzzel launcher script

---
+++ scripts/menu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+fuzzel\
+    --terminal=$1\
+    --lines=20\
+    --show-actions\
+    --tabs=4\
+    --background=110015e6\
+    --selection-color=420080ff\
+    --selection-text-color=ff00ffff\
+    --border-color=ff00cfff\
+    --border-radius=10\
+    --font='Hack Nerd Font:size=13'\
+    --dpi-aware=yes\
+    --width 60
