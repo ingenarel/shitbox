32eabf232a95dad09be4d4038c8d9ae6979b867c
ingenarel <ingenarelitems@gmail.com>
2025-06-26 17:35:05 +0600

    (sway): fix hot-reload script

--- configs/programs/WM/sway/scripts/hot-reload.sh
+++ configs/programs/WM/sway/scripts/hot-reload.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 while true; do
-    inotifywait -e close_write,modify "$HOME/.config/shitbox/configs/programs/WM/sway/config"
+    inotifywait -e close,modify "$HOME/.config/shitbox/configs/programs/WM/sway/config"
     swaymsg reload
 done
