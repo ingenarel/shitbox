ba9664860d63cb429893f9c8c7d22bcb721ad7a9
ingenarel <ingenarelitems@gmail.com>
2025-07-01 04:45:25 +0600

    (neovim): remove bloat

--- configs/programs/neovim/nvim/lua/configs/keybinds/init.lua
+++ configs/programs/neovim/nvim/lua/configs/keybinds/init.lua
@@ -8,39 +8,15 @@ end, { desc = "Saves and sources the file", silent = true, noremap = true })
 
 require("configs.keybinds.dirChange")
 
--- File managing {{{1
+-- File managing {{{
 CK_SetKeymap_n(
     "<leader>F",
     "<CMD>FloatermNew --width=0.95 --height=1.0 --title=YAZI --titleposition=center --opener=edit yazi<CR>",
     "Yazi"
 )
--- File managing }}}1
+-- File managing }}}
 
----### executes a command with floaterm.
----@param autoclosevalue 0|1 see |floaterm_autoclose|
----this can also be used to use another shell such as bash, zsh, cmd, powershell, etc
-function CK_FloatermExecuteCommand(autoclosevalue)
-    if autoclosevalue == 1 or autoclosevalue == 0 then
-    else
-        vim.notify("autoclose value can either be 0 or 1", "ERROR")
-        return nil
-    end
-
-    local command = vim.fn.input("Command")
-
-    if command == "" then
-        return nil
-    end
-
-    vim.cmd(
-        "FloatermNew --width=1.0 --height=0.9 --title=Custom_Execution --titleposition=center --autoclose="
-            .. autoclosevalue
-            .. " "
-            .. command
-    )
-end
 vim.keymap.set("t", "<M-esc>", "<C-\\><C-n>", { silent = true, noremap = true })
--- terminal }}}1
 
 vim.keymap.set("n", "grn", function()
     vim.lsp.buf.rename()
