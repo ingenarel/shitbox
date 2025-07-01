56072b2da938af253593d32fc9793120521563a2
ingenarel <ingenarelitems@gmail.com>
2025-07-01 17:59:39 +0600

    (neovim): lualine should show macro

--- configs/programs/neovim/nvim/lua/plugins/_lualine-setup_.lua
+++ configs/programs/neovim/nvim/lua/plugins/_lualine-setup_.lua
@@ -11,6 +11,12 @@ return {
                     "location",
                     "selectioncount",
                 },
+                lualine_x = {
+                    {
+                        require("noice").api.statusline.mode.get,
+                        cond = require("noice").api.statusline.mode.has,
+                    },
+                },
             },
         }
     end,
