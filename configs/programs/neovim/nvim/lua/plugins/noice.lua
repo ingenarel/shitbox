7229358ea3bce9b2130a454ed0d117a2b6b13e0a
ingenarel <ingenarelitems@gmail.com>
2025-07-01 18:00:32 +0600

    (neovim): noice cmdline and popup should show together

--- configs/programs/neovim/nvim/lua/plugins/noice.lua
+++ configs/programs/neovim/nvim/lua/plugins/noice.lua
@@ -6,6 +6,44 @@ return {
             long_message_to_split = true, -- long messages will be sent to a split
             lsp_doc_border = true,
         },
+        views = {
+            cmdline_popup = {
+                position = {
+                    row = math.floor(vim.o.lines / 2 - 10),
+                    col = "50%",
+                },
+                size = {
+                    width = 60,
+                    height = "auto",
+                },
+            },
+            popupmenu = {
+                relative = "editor",
+                position = {
+                    row = math.floor(vim.o.lines / 2 - 7),
+                    col = "50%",
+                },
+                size = {
+                    width = 60,
+                    height = 10,
+                },
+                border = {
+                    style = "rounded",
+                    padding = { 0, 1 },
+                },
+                win_options = {
+                    winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+                },
+            },
+        },
     },
-    dependencies = { "MunifTanjim/nui.nvim", { "rcarriga/nvim-notify", opts = { background_colour = "#000000" } } },
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        {
+            "rcarriga/nvim-notify",
+            opts = {
+                background_colour = "#000000",
+            },
+        },
+    },
 }
