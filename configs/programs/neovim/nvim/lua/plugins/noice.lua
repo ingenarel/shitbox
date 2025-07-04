5439b0f00f17f7b2206a74823f7b8061fdb0ea88
ingenarel <ingenarelitems@gmail.com>
2025-07-05 05:46:11 +0600

    (feat): disable noice.nvim's cmdline up thing

--- configs/programs/neovim/nvim/lua/plugins/noice.lua
+++ configs/programs/neovim/nvim/lua/plugins/noice.lua
@@ -6,18 +6,6 @@ return {
             long_message_to_split = true, -- long messages will be sent to a split
             lsp_doc_border = true,
         },
-        views = {
-            cmdline_popup = {
-                position = {
-                    row = math.floor(vim.o.lines / 2 - 10),
-                    col = "50%",
-                },
-                size = {
-                    width = 60,
-                    height = "auto",
-                },
-            },
-        },
     },
     dependencies = {
         "MunifTanjim/nui.nvim",
