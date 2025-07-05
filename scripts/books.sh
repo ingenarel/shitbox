d573e200b32615402f34eaa6ea28849e9bc60220
ingenarel <ingenarelitems@gmail.com>
2025-07-05 08:02:30 +0600

    (books): add script for the bash reference manual

--- scripts/books.sh
+++ scripts/books.sh
@@ -8,3 +8,12 @@ wget\
     --no-use-server-timestamps\
     --directory-prefix="$CParentPath"\
     "https://dn790000.ca.archive.org/0/items/c-programming-a-modern-approach-2nd-ed-c-89-c-99-king-by/C%20Programming%20-%20A%20Modern%20Approach%20-%202nd_Ed%28C89%2C%20c99%29%20-%20King%20by%20.pdf"
+
+BashReferenceManualParentPath="$HOME/Documents/books/programming/bash"
+[[ ! -d "$BashReferenceManualParentPath" ]] && mkdir --parents "$BashReferenceManualParentPath"
+ls -f "$BashReferenceManualParentPath/bash.pdf"\
+    ||
+wget\
+    --no-use-server-timestamps\
+    --directory-prefix="$CParentPath"\
+    "https://www.gnu.org/software/bash/manual/bash.pdf"
