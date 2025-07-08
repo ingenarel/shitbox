c847758e0f5b81109a1d9c69ea71c5279d7e14c1
ingenarel <ingenarelitems@gmail.com>
2025-07-08 07:19:46 +0600

    (feat): make a tmux pane renamer

---
+++ scripts/tmux-pane-renamer.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+renamePanes(){
+    _windowInfo="$(tmux display-message -p '#{session_name}:#{window_index}:#{pane_current_command}')"
+    _cmd="$( echo "$_windowInfo" | awk -F ':' '{print $3}')"
+    case "$_cmd" in
+        nvim)
+            name=""
+            ;;
+        bash|zsh|sh|fish)
+            name=""
+            ;;
+        ssh)
+            name="🖧"
+            ;;
+        man)
+            name="📖"
+            ;;
+        btop|htop|top)
+            name=""
+            ;;
+        yazi)
+            name="🗃️"
+            ;;
+        sudo|doas)
+            name="🛡️"
+            ;;
+        git|lazygit)
+            name=""
+            ;;
+        *)
+            name="$_cmd"
+            ;;
+    esac
+    tmux rename-window -t "$(echo "$_windowInfo" | awk -F ':' '{print $1 ":" $2 }')" "$name"
+}
+
+listOfProcesses="$(pgrep -f tmux-pane-renamer.sh)"
+#shellcheck disable=SC2015
+[ "$( echo "$listOfProcesses" | wc -l )" -gt "1" ] && kill "$(echo "$listOfProcesses" | head -n -1)"
+while sleep 1; do
+    tmux list-sessions > /dev/null || exit
+    renamePanes
+done
