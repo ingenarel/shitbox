4061049cedc6cd757fdd4717096636b3ee1a85d6
ingenarel <ingenarelitems@gmail.com>
2025-07-12 08:36:55 +0600

    (feat): better count lines

--- count-lines.sh
+++ count-lines.sh
@@ -1,9 +1,33 @@
 #!/usr/bin/env bash
 
-lines=0
-for file in $(git ls-files); do
+shitboxLines=0
+for file in $(git -C "$HOME/.config/shitbox" ls-files); do
     file --mime-type "$file" | grep -qE ".+ text/.+" && {
-        lines="$(( "$lines" + "$( wc -l "$file" | grep -oE "^[0-9]+")" ))"
+        shitboxLines="$(( "$shitboxLines" + "$( wc -l "$file" | grep -oE "^[0-9]+")" ))"
     }
 done
-echo "$lines"
+echo "Project shitbox lines (excluding submodules) = $shitboxLines"
+
+neovimLines=0
+for file in $(find "$HOME/.config/shitbox/configs/programs/neovim/nvim/"); do
+    file --mime-type "$file" | grep -qE ".+ text/.+" && {
+        neovimLines="$(( "$neovimLines" + "$( wc -l "$file" | grep -oE "^[0-9]+")" ))"
+    }
+done
+echo "Neovim config lines = $neovimLines"
+
+neovimPluginLines=0
+for file in $(find "$HOME/.config/shitbox/configs/programs/neovim/myPlugins/"); do
+    file --mime-type "$file" | grep -qE ".+ text/.+" && {
+        neovimPluginLines="$(( "$neovimPluginLines" + "$( wc -l "$file" | grep -oE "^[0-9]+")" ))"
+    }
+done
+echo "My neovim plugin lines = $neovimPluginLines"
+
+totalLines=0
+for file in $(find "$HOME/.config/shitbox/"); do
+    file --mime-type "$file" | grep -qE ".+ text/.+" && {
+        totalLines="$(( "$totalLines" + "$( wc -l "$file" | grep -oE "^[0-9]+")" ))"
+    }
+done
+echo "Project shitbox lines (everything including submodules) = $totalLines"
