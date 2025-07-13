7cea2b3430e13df44158ca87716ffe23d13e364a
ingenarel <ingenarelitems@gmail.com>
2025-07-13 09:21:06 +0600

    (fix): 59828931a5d7519d4a4c25eb57aff80815a7c50d

--- scripts/gentoo/deps.sh
+++ scripts/gentoo/deps.sh
@@ -9,21 +9,23 @@ ebuild "$1" manifest fetch unpack
 sourcePath="/var/tmp/portage/$(realpath "$1" | awk -F '/' '{print $(NF-2)}')/$package/work/$package"
 cd "$sourcePath" || exit
 
+depfileName="$package-deps.tar.xz"
 if [[ "$3" == "go" ]]; then
     GOMODCACHE="$sourcePath/go-mod" go mod download -modcacherw -x || {
         error="$?"
         echo "go vendor creation failed"
         exit "$error"
     }
+    tar --create --auto-compress --file "$depfileName" go-mod
 elif [[ "$3" == "rust" ]]; then
     cargo vendor || {
         error="$?"
         echo "cargo vendor creation failed"
         exit "$error"
     }
+    tar --create --auto-compress --file "$depfileName" vendor
+else
+    echo "invalid shit"
 fi
-
-depfileName="$package-deps.tar.xz"
-tar --create --auto-compress --file "$depfileName" vendor
 gh release create "$depfileName" --repo "$depfilesRepo"
 gh release upload "$depfileName" "$depfileName" --repo "$depfilesRepo"
