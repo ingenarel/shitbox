f040bf49323365a277fff7837157b78ad0abea89
ingenarel <ingenarelitems@gmail.com>
2025-07-15 02:09:04 +0600

    (secrets): update secrets

--- configs/secrets/encrypt-decrypt-files.sh
+++ configs/secrets/encrypt-decrypt-files.sh
@@ -10,6 +10,8 @@ kdbxBackupDecryptedFile="$scriptDir/passwords/PasswordsBackup.kdbx"
 kdbxBackupEncryptedFile="$scriptDir/passwords/PasswordsBackup.kdbx.gpg"
 gpgSignDecryptedFile="$scriptDir/gpg/gitSign"
 gpgSignEncryptedFile="$scriptDir/gpg/gitSign.gpg"
+neomuttDecryptedFile="$scriptDir/gpg/neomutt"
+neomuttEncryptedFile="$scriptDir/gpg/neomutt.gpg"
 
 encryptFile() {
     if [[ ! -e "$1" ]]; then
@@ -52,6 +54,7 @@ if [[ "$2" == "encrypt" ]]; then
     encryptFile "$kdbxDecryptedFile" "$kdbxEncryptedFile" "$1"
     encryptFile "$kdbxBackupDecryptedFile" "$kdbxBackupEncryptedFile" "$1"
     encryptFile "$gpgSignDecryptedFile" "$gpgSignEncryptedFile" "$1"
+    encryptFile "$neomuttDecryptedFile" "$neomuttEncryptedFile" "$1"
 elif [[ "$2" == "decrypt" ]]; then
     decryptFile "$sshDecryptedFile" "$sshEncryptedFile" "$1"
     if [[ ! -d "$HOME/.ssh" ]]; then
@@ -64,9 +67,13 @@ elif [[ "$2" == "decrypt" ]]; then
     decryptFile "$kdbxDecryptedFile" "$kdbxEncryptedFile" "$1"
     decryptFile "$kdbxBackupDecryptedFile" "$kdbxBackupEncryptedFile" "$1"
     decryptFile "$gpgSignDecryptedFile" "$gpgSignEncryptedFile" "$1"
+    decryptFile "$neomuttDecryptedFile" "$neomuttEncryptedFile" "$1"
     gpg --import "$gpgSignDecryptedFile"
     gpg --import "${gpgSignDecryptedFile}.pub"
-    gpg --edit-key "ingenarelitems@gmail.com"
+    gpg --edit-key "66D11DC0EFAC2F99D0A687F91E81128A3F325C35"
+    gpg --import "$neomuttDecryptedFile"
+    gpg --import "${neomuttDecryptedFile}.pub"
+    gpg --edit-key "82F50B33201B8D6A54127C7C884988B67CE8CCDB"
 elif [[ -z $1 || $1 == "--help" || $1 == "-h" ]]; then
     echo "$scriptDir/encrypt-decrypt-files.sh PASSWORD (encrypt|decrypt)"
 fi
