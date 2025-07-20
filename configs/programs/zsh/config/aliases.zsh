e64bae5e5a7b168b57b11973d1fd2ca927326573
ingenarel <ingenarelitems@gmail.com>
2025-07-20 08:41:34 +0600

    (zsh): lsd always tries to show icons

--- configs/programs/zsh/config/aliases.zsh
+++ configs/programs/zsh/config/aliases.zsh
@@ -1,4 +1,4 @@
-LS_COMMAND='lsd --almost-all --color always --group-directories-first'
+LS_COMMAND='lsd --almost-all --color always --group-directories-first --icon always'
 alias btop="sudo btop"
 alias discordo='discordo -token "$(gpg -d ~/.config/wofi/token.gpg)"'
 alias graph="pacgraph --svg -b #000000 -l #013d35 -t #f40cfc -d #ffffff --show-req-by --by-area --explicits --opt-deps -f ~/Images/pacgraph/pacgraph"
