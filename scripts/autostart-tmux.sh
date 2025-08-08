4f01f25010d0165a46c0fd2b6a60680378f8f714
ingenarel <ingenarelitems@gmail.com>
2025-08-08 10:39:38 +0600

    (tmux): show the tasks when opening

--- scripts/autostart-tmux.sh
+++ scripts/autostart-tmux.sh
@@ -3,5 +3,5 @@ if [[ $(tmux list-sessions) ]]; then
     tmux attach
 else
     # tmux new-session "fastfetch --logo-padding-left \$(( ( \$(tput cols) - 93) / 2 )); \$SHELL"
-    tmux
+    tmux new-session "task; $SHELL"
 fi
