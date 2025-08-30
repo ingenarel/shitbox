96923fc69e9cddbcd66655eefe670c5501ae1aed
ingenarel <ingenarelitems@gmail.com>
2025-08-31 04:06:17 +0600

    (scripts): set PROTONPATH in lutris runner

--- scripts/lutris-manager.sh
+++ scripts/lutris-manager.sh
@@ -2,6 +2,9 @@ command -v lutris >/dev/null 2>&1 || {
     echo "lutris not found"
     exit
 }
+
+export PROTONPATH="$HOME/.local/share/lutris/runners/proton/ge-proton"
+
 gameName="$(lutris --installed --list-games 2>/dev/null | awk -F '|' '{print $3}' | sed -E 's/^\s+//; s/\s+$//' | fuzzel --dmenu)"
 [[ -n "$gameName" ]] && {
     lutris "$gameName" --output-script "/tmp/$gameName.sh"
