9e5aade6ac5b21a7ba902940bb7ec445cbe8c2a8
ingenarel <ingenarelitems@gmail.com>
2025-09-12 03:01:23 +0600

    (scripts): lutris managers should use a cache now

--- scripts/lutris-manager.sh
+++ scripts/lutris-manager.sh
@@ -5,8 +5,25 @@ command -v lutris >/dev/null 2>&1 || {
 
 export PROTONPATH="$HOME/.local/share/lutris/runners/proton/ge-proton"
 
-gameName="$(lutris --installed --list-games 2>/dev/null | awk -F '|' '{print $3}' | sed -E 's/^\s+//; s/\s+$//' | fuzzel --dmenu)"
+gameName="$(
+    lutris\
+        --installed\
+        --list-games\
+        2>/dev/null\
+        |
+    awk\
+        -F '|'\
+        '{print $3}'\
+        |
+    sed\
+        -E 's/^\s+//; s/\s+$//'\
+        |
+    fuzzel\
+        --dmenu\
+        --cache="$HOME/.cache/lutris-manager-fuzzel"
+)"
 [[ -n "$gameName" ]] && {
     lutris "$gameName" --output-script "/tmp/$gameName.sh"
     "/tmp/$gameName.sh"
+    rm "/tmp/$gameName.sh"
 }
