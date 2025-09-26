9f0d573fcdfebdd59df7b3929e5429a574fec9fa
ingenarel <ingenarelitems@gmail.com>
2025-09-26 20:17:28 +0600

    (scripts): faster lutris launcher with file caching and smart cache change

--- scripts/lutris-manager.sh
+++ scripts/lutris-manager.sh
@@ -3,27 +3,56 @@ command -v lutris >/dev/null 2>&1 || {
     exit
 }
 
+command -v fuzzel >/dev/null 2>&1 || {
+    echo "fuzzel not found"
+    exit
+}
+
 export PROTONPATH="$HOME/.local/share/lutris/runners/proton/ge-proton"
+gameNamesFile="$HOME/.cache/lutris-manager-names"
+
+setGameNames(){
+    [[ -z $GAME_NAMES ]] && GAME_NAMES="$(
+        lutris\
+            --installed\
+            --list-games\
+            2>/dev/null\
+            |
+        awk\
+            -F '|'\
+            '{print $3}'\
+            |
+        sed\
+            -E 's/^\s+//; s/\s+$//'
+    )"
+}
+
+getCacheNames(){
+    [[ -z $CACHE_NAMES ]] && CACHE_NAMES="$(
+        [[ -f "$gameNamesFile" ]] && {
+            cat "$gameNamesFile"
+            } || {
+            setGameNames
+            echo "$GAME_NAMES" > "$gameNamesFile"
+            cat "$gameNamesFile"
+        }
+    )"
+}
+
+getCacheNames
 
 gameName="$(
-    lutris\
-        --installed\
-        --list-games\
-        2>/dev/null\
-        |
-    awk\
-        -F '|'\
-        '{print $3}'\
-        |
-    sed\
-        -E 's/^\s+//; s/\s+$//'\
+    echo "$CACHE_NAMES"\
         |
     fuzzel\
         --dmenu\
         --cache="$HOME/.cache/lutris-manager-fuzzel"
 )"
+
 [[ -n "$gameName" ]] && {
     lutris "$gameName" --output-script "/tmp/$gameName.sh"
     "/tmp/$gameName.sh"
-    rm "/tmp/$gameName.sh"
 }
+
+setGameNames
+[[ "$CACHE_NAMES" != "$GAME_NAMES" ]] && echo "$GAME_NAMES" > "$gameNamesFile"
