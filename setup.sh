ef2992ce28235f11d083fbcd0bcbe436f98f0d48
ingenarel <ingenarelitems@gmail.com>
2025-11-14 01:24:01 +0600

    (refactor): use if elif statements

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 scriptDir="$(realpath --canonicalize-missing "${BASH_SOURCE[0]}/..")"
 
-[[ -d /run/archiso ]] && "$scriptDir/install/arch-pre.sh" "$1" "$2" "$3"
-
-([[ $HOSTNAME == "livecd" ]] && grep -qEi "gentoo" /etc/os-release) && "$scriptDir/install/gentoo-pre.sh" "$1" "$2" "$3"
+if grep -qi 'arch' /etc/os-release && [[ -d /run/archiso ]]; then
+    "$scriptDir/install/arch-pre.sh" "$1" "$2" "$3"
+elif grep -qi 'gentoo' /etc/os-release && [[ "$HOSTNAME" == "livecd" ]]; then
+    "$scriptDir/install/gentoo-pre.sh" "$1" "$2" "$3"
+fi
