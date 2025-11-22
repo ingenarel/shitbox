0c971a781b5eeca906e4db4250e8333a9e6fe3b2
ingenarel <ingenarelitems@gmail.com>
2025-11-23 05:31:16 +0600

    (zen): delete bloat

--- configs/programs/browsers/firefox/zen/zen.js
+++ configs/programs/browsers/firefox/zen/zen.js
@@ -2,16 +2,11 @@ user_pref("browser.compactmode.show", true);
 user_pref("zen.view.compact.hide-toolbar", true);
 user_pref("zen.welcome-screen.seen", true);
 
-user_pref("browser.tabs.allow_transparent_browser", true);
-user_pref("zen.widget.linux.transparency", true);
-
 user_pref("extensions.webextensions.restrictedDomains", "");
 user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
 
 user_pref("browser.download.useDownloadDir", false);
 
-user_pref("full-screen-api.transition-duration.enter", "0 0");
-user_pref("full-screen-api.transition-duration.leave", "0 0");
 user_pref("sidebar.animation.enabled", false);
 user_pref("sidebar.animation.duration-ms", 0);
 user_pref("toolkit.cosmeticAnimations.enabled", false);
