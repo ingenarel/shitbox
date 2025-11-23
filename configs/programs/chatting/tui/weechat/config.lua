93de863553c1f5a69028b05773b99a7c74de9df8
ingenarel <ingenarelitems@gmail.com>
2025-11-23 19:02:04 +0600

    (weechat): update plugins

--- configs/programs/chatting/tui/weechat/config.lua
+++ configs/programs/chatting/tui/weechat/config.lua
@@ -1,7 +1,18 @@
 weechat.register("test_lua", "ingenarel", "1.0", "GPL3", "Test script", "", "")
 weechat.print("", "Hello, from lua script!")
 
-for _, plugin in ipairs({ "autosort.py", "vimode.py", "atcomplete.pl", "cmdind.pl", "lnotify.py", "emoji.lua" }) do
+for _, plugin in ipairs({
+	"aesthetic.py",
+	"aformat.py",
+	"atcomplete.pl",
+	"autosort.py",
+	"cmdind.pl",
+	"colorize_nicks.py",
+	"emoji.lua",
+	"go.py",
+	"lnotify.py",
+	"vimode.py",
+}) do
 	weechat.print("", "Hello, from lua script!" .. plugin)
 	weechat.command("", "/script install " .. plugin)
 	weechat.command("", "/script load " .. plugin)
