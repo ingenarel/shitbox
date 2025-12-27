47a8e722c0ca30f7c6e0802e835debf29bbbe633
ingenarel <ingenarelitems@gmail.com>
2025-12-27 17:13:35 +0600

    (librewolf): add a user.js

---
+++ configs/programs/browsers/firefox/librewolf/user.js
@@ -0,0 +1,7 @@
+user_pref("sidebar.revamp", true);
+user_pref("sidebar.verticalTabs", true);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("sidebar.visibility", "expand-on-hover");
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.uiCustomization.navBarWhenVerticalTabs", "")
+user_pref("browser.uiCustomization.state", "")
