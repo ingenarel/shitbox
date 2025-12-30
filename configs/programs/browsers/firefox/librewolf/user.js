8a4576264d205c70e6b773ccd84284794f4c4332
ingenarel (NeoJesus) <ingenarel_neojesus@disroot.org>
2025-12-31 00:59:27 +0600

    (librewolf): fix `;` for user.js

--- configs/programs/browsers/firefox/librewolf/user.js
+++ configs/programs/browsers/firefox/librewolf/user.js
@@ -3,5 +3,5 @@ user_pref("sidebar.verticalTabs", true);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 user_pref("sidebar.visibility", "expand-on-hover");
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.uiCustomization.navBarWhenVerticalTabs", "")
-user_pref("browser.uiCustomization.state", "")
+user_pref("browser.uiCustomization.navBarWhenVerticalTabs", "");
+user_pref("browser.uiCustomization.state", "");
