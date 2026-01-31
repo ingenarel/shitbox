b882ba15e26a193bf54332ac2debaddda739116d
ingenarel (NeoJesus) <ingenarel_neojesus@disroot.org>
2026-01-31 12:11:04 +0600

    feat(neovim): generate all ebuild and eclass vars for shellcheck

--- configs/programs/neovim/nvim/lua/plugins/lsp/bashls-ebuild/create.sh
+++ configs/programs/neovim/nvim/lua/plugins/lsp/bashls-ebuild/create.sh
@@ -1,30 +1,85 @@
 #!/usr/bin/sh
 
 scriptDir="$(realpath --canonicalize-missing "$0/..")"
-ebuildHTML="$scriptDir/ebuild.5.html"
+
+ebuildHTML="$scriptDir/ebuild-vars.html"
 
 endingTagSearch=false
 {
     [ ! -f "$ebuildHTML" ] || [ "$1" = true ]
 } &&
-curl --silent 'https://dev.gentoo.org/~zmedico/portage/doc/man/ebuild.5.html' > "$scriptDir/ebuildHTML"
+curl --silent 'https://devmanual.gentoo.org/ebuild-writing/variables/index.html' > "$ebuildHTML"
 
 while IFS='' read -r line; do
     $endingTagSearch && {
         case "$line" in
-            '<H3>'*)
-                exit
+            '</table>')
+                endingTagSearch=false
                 ;;
             *)
                 echo "$line"
                 ;;
         esac
         } || {
-        [ "$line" = '<H3>Variables Used In Ebuilds</H3>' ] && {
-            echo "$line"
-            endingTagSearch=true
-        }
+        case "$line" in
+            '<h2 id="predefined-read-only-variables">'*)
+                echo "$line"
+                endingTagSearch=true
+                ;;
+            '<h2 id="ebuild-defined-variables">'*)
+                echo "$line"
+                endingTagSearch=true
+                ;;
+            '<h2 id="user-environment">'*)
+                echo "$line"
+                endingTagSearch=true
+                ;;
+        esac
     }
 done < "$ebuildHTML" |
-tail -n +4 |
-sed -nE 's/^<DT><B>([^<]+)<.*/export \1/p' > "$scriptDir/ebuild-vars.sh"
+sed -nE 's|^\s*<td><code class="docutils literal"><span class="pre">([A-Z][^<]+)</span></code></td>$|export \1|p' > "$scriptDir/ebuild-vars.sh"
+
+eclassParse(){
+    eclassHTML="$scriptDir/$1-eclass.html"
+
+    endingTagSearch=false
+    {
+        [ ! -f "$eclassHTML" ] || [ "$1" = true ]
+    } &&
+    curl --silent "https://devmanual.gentoo.org/eclass-reference/$1.eclass/index.html" > "$eclassHTML"
+
+    while IFS='' read -r line; do
+        $endingTagSearch && {
+            case "$line" in
+                '<H2'*)
+                    endingTagSearch=false
+                    ;;
+                *)
+                    echo "$line"
+                    ;;
+            esac
+            } || {
+            case "$line" in
+                '<H2 ID="'*'">ECLASS VARIABLES</H2>')
+                    echo "$line"
+                    endingTagSearch=true
+                    ;;
+            esac
+        }
+    done < "$eclassHTML" |
+    sed -nE 's|^<DT><B>([^<]+)<.+|export \1|p' >> "$scriptDir/ebuild-vars.sh"
+}
+
+eclassGroupsHTML="$scriptDir/eclass-list.html"
+
+endingTagSearch=false
+{
+    [ ! -f "$eclassGroupsHTML" ] || [ "$1" = true ]
+} &&
+curl --silent "https://devmanual.gentoo.org/eclass-reference/index.html" > "$eclassGroupsHTML"
+
+sed -nE 's|<li class="list-group-item"><a href="([^"]+)\.eclass.*|\1|p' "$eclassGroupsHTML" | while IFS='' read -r eclass; do
+    eclassParse "$eclass"
+done
+
+sed -i -E '/^export .*[&;].*/d' "$scriptDir/ebuild-vars.sh"
