4c1ff1a910bb9ead7fce10c8f708c09cb9ef6e14
ingenarel (NeoJesus) <ingenarel_neojesus@disroot.org>
2026-02-13 03:15:21 +0600

    feat(tmux): update chat script

--- configs/programs/terminal/startups/tmux-chat.sh
+++ configs/programs/terminal/startups/tmux-chat.sh
@@ -2,11 +2,15 @@
 if tmux list-sessions | grep -q chat; then
     exec tmux attach -t chat
 else
-    exec tmux new-session -d -s 'chat' "$SHELL" -c "torsocks aerc"\
-        \;\
-        new-window "$SHELL" -c "weechat"\
-        # \;\
-        # new-window "$SHELL" -c "iamb"\
-        # \;\
-        # new-window "$SHELL" -c "discordo"
+    exec tmux new-session -d -s 'chat' torsocks aerc\;\
+        rename-window -t chat:0 '📧'\;\
+        new-window weechat\;\
+        rename-window -t chat:1 '󰰭'\;\
+        new-window torsocks oxicord --enable-desktop-notifications true --group-guilds true\;\
+        rename-window -t chat:2 ''\;\
+        new-window iamb\;\
+        rename-window -t chat:3 '󰯊'
+    # new-window env -i HOME="$HOME" PATH="$PATH" TERM=tmux-256color sh -i -l -c 'env; sleep 99999'\;
+    # new-window env -i HOME="$HOME" sh -l -c 'iamb'\;\
+        # new-window torsocks oxicord --enable-desktop-notifications true --group-guilds true
 fi
