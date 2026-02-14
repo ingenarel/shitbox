7fdd40fe47a79cc9051d4c252c71cb1f1d0c8b10
ingenarel (NeoJesus) <ingenarel_neojesus@disroot.org>
2026-02-14 09:09:38 +0600

    fix: vesktop .desktop file when using -bin pkg in gentoo

--- configs/programs/chatting/gui/discord/start-vesktop.sh
+++ configs/programs/chatting/gui/discord/start-vesktop.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/sh
 
+command -v vesktop && VESKTOP_COMMAND=vesktop || command -v vesktop-bin && VESKTOP_COMMAND=vesktop-bin || exit 1
+
 systemctl --quiet is-active mpd-discord-rpc && RPC_ACTIVE=true
 
 if [ "$RPC_ACTIVE" = true ]; then
-    exec vesktop "$@"
+    exec $VESKTOP_COMMAND "$@"
 else
     systemctl --quiet --user start mpd-discord-rpc
-    vesktop "$@"
+    $VESKTOP_COMMAND "$@"
     systemctl --user stop mpd-discord-rpc
 fi
 
